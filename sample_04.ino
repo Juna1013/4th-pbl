033618b869493779a0e46ffa37504ee833c4d48d
Junya <160022550+Juna1013@users.noreply.github.com>
2025-04-15 10:13:25 +0900

    Create sample_04.ino

---
+++ sample_04.ino
@@ -0,0 +1,30 @@
+// 起動時の1回のみ実行
+void setup() {
+  pinMode(16, OUTPUT);
+  pinMode(17, OUTPUT);
+}
+
+// 繰り返し実行
+void loop() {
+  clockwise1();
+  delay(1000);
+  clockwise2();
+  delay(1000);
+  clockwise3();
+  delay(1000);
+}
+
+void clockwise1() {
+  analogWrite(16, 0);
+  analogWrite(17, 0);
+}
+
+void clockwise2() {
+  analogWrite(16, 128);
+  analogWrite(17, 0);
+}
+
+void clockwise3(){
+  analogWrite(16, 255);
+  analogWrite(17, 0);
+}
